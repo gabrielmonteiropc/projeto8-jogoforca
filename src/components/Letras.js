06d05924ec384d7257659abb652d223ab91d75ca
Gabriel <bhcmonteiro54@gmail.com>
2023-07-10 15:12:34 -0300

    Tentando desabilitar os botões ao iniciar jogo

--- src/components/Letras.js
+++ src/components/Letras.js
@@ -1,4 +1,4 @@
-export default function Letras({quantidadeErros, setquantidadeErros, sortearJogo, setsortearJogo, setcorPalavra, setDesabilitar, palavraSorteada, letras, setLetras}) {
+export default function Letras({quantidadeErros, setquantidadeErros, sortearJogo, setsortearJogo, setcorPalavra, setDesabilitar, palavraSorteada, letras, setLetras, desabilitar}) {
 
     const palavrasAlfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
